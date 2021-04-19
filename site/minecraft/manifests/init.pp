181238e952939bd53934a5c4d64eda733e3eeab4
p.forty <petr@pforty.cz>
2021-04-19 13:20:22 +0200

    template -> content

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -23,7 +23,7 @@ class minecraft (
 
   file { '/etc/systemd/system/minecraft.service':
     ensure => file,
-    template => template('minecraft/minecraft.service.erb'),
+    content => template('minecraft/minecraft.service.erb'),
   }
 
   service { 'minecraft':
